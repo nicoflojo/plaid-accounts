dfc82d1ace232c0228def54955125eb295c85e30
Nico <nico@skubos.com>
2019-09-25 15:53:29 +0700

    adds user Schema

---
+++ models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  pasword: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    defualt: Date.now
+  }
+});
+
+module.exports = User = mongoose.model('users', UserSchema);
