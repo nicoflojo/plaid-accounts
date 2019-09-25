ea03db70f8cab2f34c29c2339c65847f296aa383
Nico <nico@skubos.com>
2019-09-25 22:21:42 +0700

    login post route

--- routes/api/users.js
+++ routes/api/users.js
@@ -10,6 +10,9 @@ const validateLoginInput = require('../../validation/login');
 const User = require('../../models/User');
 
 router.post('/register', (req, res) => {
+
+  const { errors, isValid } = validateRegisterInput(req.body);
+
   if (!isValid) {
     return res.status(400).json(errors);
   }
@@ -37,3 +40,46 @@ router.post('/register', (req, res) => {
       }));
     });
 });
+
+router.post("/login", (req, res) => {
+
+  const { errors, isValid } = validateLoginInput(req.body);
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({ email }).then(user => {
+    if (!user) {
+      return res.status(404).json({ emailnotfound: "Email not found" });
+    }
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        const payload = {
+          id: user.id,
+          name: user.name
+        };
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          {
+            expiresIn: 31556926 // 1 year in seconds
+          },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: "Bearer " + token
+            });
+          }
+        );
+      } else {
+        return res
+          .status(400)
+          .json({ passwordincorrect: "Password incorrect" });
+      }
+    });
+  });
+});
+
+module.exports = router;
