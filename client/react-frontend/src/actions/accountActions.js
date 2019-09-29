bee37ec375a2bcfca3bd1618a338a18c1fb1d26d
Nico <nico@skubos.com>
2019-09-29 14:23:22 +0700

    addAccount action

--- client/react-frontend/src/actions/accountActions.js
+++ client/react-frontend/src/actions/accountActions.js
@@ -11,3 +11,19 @@ import {
 
 // Actions here
 
+const addAccount = plaidData = dispatch => {
+  const accounts = plaidData.accounts;
+  axios
+    .post('/api/plaid/accounts/add', plaidData)
+    .then(res =>
+      dispatch({
+        type: ADD_ACCOUNT,
+        payload: res.data
+      })
+    )
+    .then(data =>
+      accounts ?
+        dispatch(getTransactions(accounts.concat(data.payload))) : null
+    )
+    .catch(err => console.log(err));
+};
