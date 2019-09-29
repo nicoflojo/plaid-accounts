5b0e8520c6cb83b19c0adbb9bc88370236a9752b
Nico <nico@skubos.com>
2019-09-29 15:50:38 +0700

    adds delete getAccounts getTransactions Actions

--- client/react-frontend/src/actions/accountActions.js
+++ client/react-frontend/src/actions/accountActions.js
@@ -27,3 +27,58 @@ const addAccount = plaidData = dispatch => {
     )
     .catch(err => console.log(err));
 };
+
+export const deleteAccount = plaidData => dispatch => {
+  if (window.confirm('Are you sure you want to remove account?')) {
+    const id = plaidData.id;
+    const newAccounts = plaidData.accounts.filter(
+      account => account._id !== id
+    );
+    axios
+      .delete(`/api/plaid/accounts/${id}`)
+      .then(res =>
+        dispatch({
+          type: DELETE_ACCOUNT,
+          payload: id
+        })
+      )
+      .then(newAccounts ? dispatch(getTransactions(newAccounts)) : null)
+      .catch(err => console.log(err));
+  }
+};
+
+export const getAccounts = () => dispatch => {
+  dispatch(setAccountsLoading());
+  axios
+    .get('/api/plaid/accounts')
+    .then(res =>
+      dispatch({
+        type: getAccounts,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ACCOUNTS,
+        payload: null
+      })
+    );
+};
+
+export const getTransactions = plaidData => dispatch => {
+  dispatch(setTransactionsLoading());
+  axios
+    .post('/api/plaid/accounts/transactions', plaidData)
+    .then(res =>
+      dispatch({
+        type: GET_TRANSACTIONS,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_TRANSACTIONS,
+        payload: null
+      })
+    );
+};
